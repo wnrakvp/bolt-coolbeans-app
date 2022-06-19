2eb21d5b5c8da805d6d5feefbea5f2a046853a63
wnrakvp <wnr.akvp@gmail.com>
2022-06-19 14:21:08 +0700

    ModalInSlack

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = new App({
 
 app.message('hello', async({message,say}) => {
   await say({
+    "text": 'Hi',
     "blocks": [
       {
         "type": "section",
@@ -34,7 +35,8 @@ app.message('hello', async({message,say}) => {
               "text": "Americano",
               "emoji": true
             },
-            "value": "americano"
+            "value": "americano",
+            "action_id": "button_click"
           },
           {
             "type": "button",
@@ -43,7 +45,8 @@ app.message('hello', async({message,say}) => {
               "text": "Latte",
               "emoji": true
             },
-            "value": "latte"
+            "value": "latte",
+            "action_id": "button_click2"
           },
           {
             "type": "button",
@@ -52,18 +55,187 @@ app.message('hello', async({message,say}) => {
               "text": "C.Mac",
               "emoji": true
             },
-            "value": "C.mac"
+            "value": "C.mac",
+            "action_id": "button_click3"
           }
-        ]
+        ],
+        // "action_id": "button_click"
       }
     ]
   });
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
+app.action('updateStock', async ({ ack, body, client, logger }) => {
   // Acknowledge the action
   await ack();
-  await say(`We received <@${body.user.id}> request. Please Wait for moment!`);
+  try {
+    // Call views.open with the built-in client
+    const result = await client.views.open({
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'input_c',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+
+  // Listen for a button invocation with action_id `button_abc` (assume it's inside of a modal)
+app.action('button_abc', async ({ ack, body, client, logger }) => {
+  // Acknowledge the button request
+  await ack();
+
+  try {
+    // Call views.update with the built-in client
+    const result = await client.views.update({
+      // Pass the view_id
+      view_id: body.view.id,
+      // Pass the current hash to avoid race conditions
+      hash: body.view.hash,
+      // View payload with updated blocks
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Updated modal'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: 'You updated the modal!'
+            }
+          },
+          {
+            type: 'image',
+            image_url: 'https://media.giphy.com/media/SVZGEcYt7brkFUyU90/giphy.gif',
+            alt_text: 'Yay! The modal was updated'
+          }
+        ]
+      }
+    });
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+});
+
+// Update the view on submission 
+app.view('view_1', async ({ ack , body}) => {
+  const userInput = body.view.state.values.input_c.dreamy_input.value;
+  await ack({
+    response_action: 'update',
+    view: {
+      type: 'modal',
+      // View identifier
+      callback_id: 'view_1',
+      title: {
+        type: 'plain_text',
+        text: 'Successfully !!'
+      },
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'plain_text',
+            text: `Your dream is ${userInput}`
+          }
+        },
+      ]
+    },
+  });
+});
+// Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebar
+app.event("app_home_opened", async ({ payload,client }) => {
+  const userId = payload.user;
+  
+  try {
+    // Call the views.publish method using the WebClient passed to listeners
+    const result = await client.views.publish({
+      user_id: userId,
+      view: {
+        // Home tabs must be enabled in your app configuration page under "App Home"
+        "type": "home",
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Click button to update Stock."
+            },
+            "accessory": {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Click Me",
+                "emoji": true
+              },
+              "value": "updateStock",
+              "action_id": "updateStock"
+            }
+          }
+        ]
+      }
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+app.error(async (error) => {
+  // Check the details of the error to handle cases where you should retry sending a message or stop the app
+  console.error(error);
 });
 
 (async () => {
