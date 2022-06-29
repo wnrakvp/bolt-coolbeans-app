363e6945fc020899c3c8443d461b2de71a374454
wnrakvp <wnr.akvp@gmail.com>
2022-06-29 12:02:37 +0700

    UpdateGoogleSheetLogicv1

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
-const { App } = require('@slack/bolt');
-const dotenv = require('dotenv');
-const { google } = require('googleapis');
-const authenticateUser = require('./middleware/auth')
-const { homeView } = require('./views/home')
-const { modalView, updateView } = require('./views/ticket')
+const { App } = require("@slack/bolt");
+const dotenv = require("dotenv");
+const { google } = require("googleapis");
+const authenticateUser = require("./middleware/auth");
+const { homeView } = require("./views/home");
+const { modalView, updateView } = require("./views/ticket");
 
 // Config File
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 // Google Credentials
 const authGoogle = new google.auth.GoogleAuth({
-  keyFile: './config/credentials.json',
-  scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  keyFile: "./config/credentials.json",
+  scopes: "https://www.googleapis.com/auth/spreadsheets",
 });
 const clientGoogle = async () => await auth.getClient();
-const googleSheets = google.sheets({ version: 'v4', auth: clientGoogle });
+const googleSheets = google.sheets({ version: "v4", auth: clientGoogle });
 const spreadsheetId = process.env.GOOGLESHEET_ID;
 
 // Query from Google Sheet
 // Read rows from spreadsheet
 async function queryGoogleSheet(payload) {
-const getRows = await googleSheets.spreadsheets.values.get({
-  auth: authGoogle,
-  spreadsheetId,
-  range: 'Stock Calculation!B:E',
-});
-const stocks = getRows.data.values.filter((item) => {
-  if (item.includes(payload.selected_option.value)) {
-    return item;
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth: authGoogle,
+    spreadsheetId,
+    range: "Stock Calculation!B:E",
+  });
+  // const responses = getRows.data.values.forEach((response) => {
+  //   console.log(response.indexOf(payload.selected_option.value));
+  // });
+  const stocks = getRows.data.values.filter((item) => {
+    if (item.includes(payload.selected_option.value)) {
+      return item;
+    }
+  });
+  return stocks;
+}
+// Read and Update from spreadsheet
+async function updateGoogleSheet() {
+  try {
+    const response = await googleSheets.spreadsheets.values.batchUpdate({
+      auth: authGoogle,
+      spreadsheetId,
+      requestBody: {
+        valueInputOption: "USER_ENTERED",
+        data: [
+          {
+            range: "Stock Calculation!A49:B49",
+            values: [["Test", "Test"]],
+          },
+        ],
+      },
+    });
+    // const response = await googleSheets.spreadsheets.developerMetadata.get({
+    //   auth: authGoogle,
+    //   spreadsheetId,
+    //   metadataId: 1,
+    // });
+    console.log(response)
+    return response;
+  } catch (e) {
+    console.log(e.message);
   }
-});
-return stocks;
 }
 // Initialize your app
 const app = new App({
@@ -40,69 +70,96 @@ const app = new App({
   port: process.env.PORT || 3000,
 });
 // Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebar
-app.event('app_home_opened',authenticateUser, async ({client, logger}) => {
-  const currentStock = '-';
-  const currentAmount = '-';
-  const currentPrice = '-';
-  const totalPrice = '0';
-  const lastUpdated = '-';
+app.event("app_home_opened", authenticateUser, async ({ client, logger }) => {
+  const currentStock = "-";
+  const currentAmount = "-";
+  const currentPrice = "-";
+  const totalPrice = "0";
+  const lastUpdated = "-";
   try {
-  await homeView(userId, client, currentStock, currentAmount, currentPrice, totalPrice, lastUpdated);
-  } catch(e) {
-  logger.error(e.message);
-  }
-});
-app.action('getStocks', async ({ ack, payload, client, logger }) => {
-  try {
-  // Acknowledge the action
-  await ack();
-  logger.info(
-    `Looking for ${payload.selected_option.value} Type in Google Sheet`
-  );
-  let currentStock = '';
-  let currentAmount = '';
-  let currentPrice = '';
-  let totalPrice = 0;
-  let lastUpdated = '';
-  const stocks = await queryGoogleSheet(payload);
-  stocks.forEach((stock) => {
-    currentStock += `${stock[0]}\n`;
-    currentAmount += `${stock[2]}\n`;
-    currentPrice += `0 Baht\n`;
-    totalPrice += stock[3]*stock[2];
-    lastUpdated =  stock[3];
-  });
-  await homeView(userId, client, currentStock, currentAmount, currentPrice, totalPrice, lastUpdated);
-  logger.info(
-    `User: ${userId} has viewed your ${payload.selected_option.value} Stocks`
-  );
+    updateGoogleSheet();
+    await homeView(
+      userId,
+      client,
+      currentStock,
+      currentAmount,
+      currentPrice,
+      totalPrice,
+      lastUpdated
+    );
   } catch (e) {
     logger.error(e.message);
   }
 });
-app.command('/update', async ({ ack, body, client, logger }) => {
+
+app.action("getStocks", async ({ ack, payload, client, logger }) => {
+  try {
+    // Acknowledge the action
+    await ack();
+    logger.info(
+      `Looking for ${payload.selected_option.value} Type in Google Sheet`
+    );
+    let currentStock = "";
+    let currentAmount = "";
+    let currentPrice = "";
+    let totalPrice = 0;
+    let lastUpdated = "";
+    const stocks = await queryGoogleSheet(payload);
+    stocks.forEach((stock) => {
+      currentStock += `${stock[0]}\n`;
+      currentAmount += `${stock[2]}\n`;
+      currentPrice += `0 Baht\n`;
+      totalPrice += stock[3] * stock[2];
+      lastUpdated = stock[3];
+    });
+    await homeView(
+      userId,
+      client,
+      currentStock,
+      currentAmount,
+      currentPrice,
+      totalPrice,
+      lastUpdated
+    );
+    logger.info(
+      `User: ${userId} has viewed your ${payload.selected_option.value} Stocks`
+    );
+  } catch (e) {
+    logger.error(e.message);
+  }
+});
+
+app.command("/update", async ({ ack, body, client, logger }) => {
   // Acknowledge command request
   await ack();
-  await modalView(body, client, logger)
+  await modalView(body, client, logger);
   // await say(`${command.text}`);
 });
-app.action('selectStock', async({ack, body, payload, client, logger }) => {
+
+app.action("selectStock", async ({ ack, body, payload, client, logger }) => {
   await ack();
-  console.log(payload.selected_option.value)
+  // console.log(payload.selected_option.value);
   const stocks = await queryGoogleSheet(payload);
-  console.log(stocks);
+  // console.log(stocks);
   const items = {};
   stocks.forEach((stock) => {
     console.log(stock);
     type = stock[1];
     updated = stock[3];
     items[stock[0]] = stock[2];
-  })
-  console.log(items);
+  });
+  // console.log(items);
   await updateView(body, client, logger, type, updated, items);
 });
 
-app.view('updatestock', async ({ack}) => {
+app.view("updatestock", async ({ ack, payload }) => {
+  await ack();
+  Object.entries(payload.state.values).forEach((item) => {
+    console.log(item[1].updateAmount.selected_option.value);
+  });
+});
+
+app.action("updateAmount", async ({ ack }) => {
   await ack();
 });
 
@@ -114,5 +171,5 @@ app.error(async (error) => {
 (async () => {
   // Start your App
   await app.start(process.env.PORT || 3000);
-  console.log('Bolt app is running!');
+  console.log("Bolt app is running!");
 })();
