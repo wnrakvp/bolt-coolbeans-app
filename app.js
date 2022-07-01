0c2a6ef7262a25408be7d62099ba273c19687b70
WannaratA <wnr.akvp@gmail.com>
2022-07-01 17:25:09 +0700

    On HerokuV1

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const spreadsheetId = process.env.GOOGLESHEET_ID;
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+  // socketMode: true,
+  // appToken: process.env.SLACK_APP_TOKEN,
   port: process.env.PORT || 3000,
 });
 
